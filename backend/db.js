0cb7536ef318ab7a445c77a51bbd2ddb65baec6e
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2023-04-16 12:08:47 +0530

    User Route & Model Created.

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb+srv://<username>:<password>@cluster0.oiguh.mongodb.net/?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://<username>:<password>@cluster0.oiguh.mongodb.net/gofoodmern?retryWrites=true&w=majority";
 
 const connectToMongoDB = async () => {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true }, (error, result) => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (error, result) => {
       if (error) console.log("---", error);
       else {
         console.log("Connected Successfully!");
+        const fetched_data = await mongoose.connection.db.collection("foodItems")
+        fetched_data.find({}).toArray(function(error, data){
+          if (error) console.log(error);
+          else {
+            // console.log(data);
+          }
+        })
       }
     });
 }
