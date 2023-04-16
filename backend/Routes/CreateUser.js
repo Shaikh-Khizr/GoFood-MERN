2442062ca05db48ecb21554195cbf2667ffb58db
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2023-04-16 12:57:38 +0530

    Create User Validation Done.

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-router.post("/createUser", async (req, res) => {
+router.post("/createUser", [
+    body('name', 'Name must be at least 3 characters!').isLength({ min: 3 }),
+    body('email', 'Please enter valid email address!').isEmail(),
+    body('password', 'Password must be at least 8 characters!').isLength({ min: 8 }),
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     try {
         await User.create({
             name: req.body.name,
