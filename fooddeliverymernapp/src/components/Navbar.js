60566ee7fc9a0632bb7dff4acde346a3c63e5dbe
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2023-04-19 00:08:59 +0530

    Bug Fixed.

--- fooddeliverymernapp/src/components/Navbar.js
+++ fooddeliverymernapp/src/components/Navbar.js
@@ -12,6 +12,7 @@ export default function Navbar() {
   
   const handleLogout = () => {
     localStorage.removeItem("authToken");
+    localStorage.removeItem("userEmail");
     navigate("/login");
   }
   
